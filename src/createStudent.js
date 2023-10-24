799b65d557f39d647aabe4c7b201888dd9ad41a7
Diogo Pinto <dpintobarbosa@gmail.com>
2023-10-24 09:48:26 -0300

    resolve requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const person = () => 'Eita pessoa boa!';
+  const student = {
+    name,
+    feedback: person,
+  };
+  return student;
+};
 
 module.exports = createStudent;
